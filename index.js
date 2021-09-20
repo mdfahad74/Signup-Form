5acd0dc0502c36067b41dbdb5a923e1d6f7cbe39
mdfahad740 <md.fahad740@gmail.com>
2021-09-21 02:00:04 +0530

    CDN Renewed

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 
 $('.tab a').on('click', function (e) {
-
   e.preventDefault();
-
   $(this).parent().addClass('active');
   $(this).parent().siblings().removeClass('active');
-
   target = $(this).attr('href');
-
   $('.tab-content > div').not(target).hide();
-
   $(target).fadeIn(600);
-
 });
 
 $(".sign").on('click',function(){
